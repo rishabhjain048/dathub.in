8943e684326e64d29b31c88cdc551e0326c6d9a3
Mukul Rai <rishabhjain88712@gmail.com>
2025-02-07 08:47:23 +0530

    mukul

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Facebook, Twitter, Instagram, Linkedin, Youtube } from 'lucide-react';
+import {Twitter, Instagram, Linkedin } from 'lucide-react';
 import Image from 'next/image';
 
 export default function Footer() {
